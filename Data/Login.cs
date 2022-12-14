a927403b3234e565dc48e64a8dc69ec4a5c31aca
jonatasx <jonatas.santospessoa@gmail.com>
2022-12-14 16:59:08 -0300

    Upando Blazor com CRUD completo

---
+++ Data/Login.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using Newtonsoft.Json;
+
+namespace VoeAirlinesBlazor.Data
+{
+    public class Login
+    {
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+
+        [JsonProperty("Usuario")]
+        public string? Usuario { get; set; }
+
+        [JsonProperty("DataCriacao")]
+        public DateTime DataCriacao { get ; set; }
+
+        [JsonProperty("DataAtual")]
+        public DateTime DataAtual { get; set; } = DateTime.Now;
+
+        [JsonProperty("Senha")]
+        public string? Senha { get; set; }
+    }
+}
---
+++ Pages/Services/AppData.cs
@@ -0,0 +1,13 @@
+﻿namespace VoeAirlinesBlazor.Pages.Services
+{
+    public class AppData
+    {
+        public int Id { get; set; }
+
+        public string? Usuario { get; set; }
+
+        public string? Senha { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+    }
+}
